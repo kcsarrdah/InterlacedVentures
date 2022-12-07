86354b9659bd957bf4e5c229b9c2ca4b995c0e4a
kcsarrdah <sarrdah.k@northeastern.com>
2022-12-06 22:34:27 -0500

    jobs directory done

---
+++ interlacedVentures/src/models/JobsDirectory.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author kcsar
+ */
+public class JobsDirectory {
+    private static ArrayList<jobs> jobsDir;
+    private static JobsDirectory mInstance;
+    
+        private JobsDirectory() {
+        this.jobsDir = new ArrayList();
+        }
+        
+        public ArrayList<jobs> getJobsDir() {
+            return jobsDir;
+    }
+    
+    public void addJob(jobs job){
+        jobsDir.add(job);
+        Statement stmt;
+        try {
+            stmt = DatabaseConnectionClass.getInstance().getCon().createStatement();
+            String query1 = "INSERT INTO BusinessUsers" + " VALUES(?,?,?,?,?,?,?)";
+            java.sql.Date sqlDate = new java.sql.Date(job.getDateOfListing().getTime());
+            PreparedStatement pst = DatabaseConnectionClass.getInstance().getCon().prepareStatement(query1);
+            pst.setString(1, job.getDescription());
+            pst.setString(2, job.getRole());
+            pst.setString(3, job.getStatus());
+            pst.setDate(4, sqlDate);
+
+            int rs = pst.executeUpdate();
+            if(rs>0)
+            {
+                JOptionPane.showMessageDialog(null,"Inserted Successfully!");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Cannot be Inserted");
+        }
+    }
+    
+    public void getBusinessUserData() {
+        Statement stmt;
+        try{
+            stmt = DatabaseConnectionClass.getInstance().getCon().createStatement();
+            String str = "Select * from BusinessUsers";
+            ResultSet rs = stmt.executeQuery(str);
+            while(rs.next()) {
+               jobs job = new jobs(
+                       rs.getDate("DateOfJoining"),
+                       rs.getString("Status"),
+                       rs.getString("Description"),
+                       rs.getString("Role"));
+                
+                jobsDir.add(job);
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,"Cannot be loaded");
+        }
+    }
+    
+//    public void docData(int stateID) {
+//        for(int j=0;j<doctorDir.size();j++) {
+//            System.out.println("sdj");
+//            DoctorMainFrame doc = new DoctorMainFrame();
+//            if(LoginDirectory.getInstance().getLoginDir().get(j).getStateID() == DoctorDirectory.getInstance().getDoctorDir().get(j).getStateID()) {
+//                doc.getDoc(DoctorDirectory.getInstance().getDoctorDir().get(j));
+//            }
+//        }
+//    }
+    
+    public static JobsDirectory getInstance() {
+        if(mInstance == null)
+            mInstance = new JobsDirectory();
+        return mInstance;
+    }
+}
--- interlacedVentures/src/models/jobs.java
+++ interlacedVentures/src/models/jobs.java
@@ -4,10 +4,54 @@
  */
 package models;
 
+import java.util.Date;
+
 /**
  *
  * @author kcsar
  */
 public class jobs {
+    private String Description;
+    private String role;
+    private String status;
+    private Date dateOfListing;
     
+    public jobs(Date dateOfListing, String status, String Description, String role) {
+        this.Description = Description;
+        this.role = role;
+        this.status = status;
+        this.dateOfListing = dateOfListing;
+    }
+    
+    public Date getDateOfListing() {
+        return dateOfListing;
+    }
+
+    public void setDateOfListing(Date dateOfListing) {
+        this.dateOfListing = dateOfListing;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
