3d9236378ca11b582284cfaeac020bb29c4b8ec7
kcsarrdah <sarrdah.k@northeastern.com>
2022-12-11 17:41:09 -0500

    business user application directory

--- interlacedVentures/src/models/ApplicationBusiness.java
+++ interlacedVentures/src/models/ApplicationBusiness.java
@@ -18,7 +18,8 @@ public class ApplicationBusiness extends business{
             String address,
             String website, 
             String ownerName, 
-            String licensePath) {
+            String licensePath,
+            String appStatus) {
         super(Password, name, email, number, address, website, ownerName, licensePath);
     }
 
